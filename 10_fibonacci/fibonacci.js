01a8b2b69ace34fe3da4c87b178d8ce727957469
Simone Schiavo <simoneschiavo@icloud.com>
2024-04-25 20:43:23 +0200

    Add solution for exercises 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (val) {
+    let fibonacciArr = [1, 1];
+
+    if (+val < 0) {
+        return 'OOPS';
+    };
+
+    if (+val === 0) {
+        return 0;
+    };
+
+    if (+val === 1) {
+        return 1;
+    };
+
+    for (let i = 2; i <= +val; i++) {
+        fibonacciArr[i] = fibonacciArr[i - 2] + fibonacciArr[i - 1];
+    };
+
+    return fibonacciArr[+val - 1];
 
 };
 
