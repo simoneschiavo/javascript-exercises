a801d38fdf7eb7a68009527625ca91edef230d23
Simone Schiavo <simoneschiavo@icloud.com>
2024-04-25 16:05:21 +0200

    Add solution for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = 0;
+  const allArgsArr = args.flat();
+  allArgsArr.forEach(arg => sum += arg);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  const allArgsArr = args.flat();
+  let subtraction = allArgsArr[0];
+  for (let i = 1; i < allArgsArr.length; i++) {
+    subtraction -= allArgsArr[i];
+  };
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	let sum = 0;
+  const allArgsArr = args.flat();
+  allArgsArr.forEach((arg) => (sum += arg));
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let multiplication = 1;
+  const allArgsArr = args.flat();
+  allArgsArr.forEach((arg) => (multiplication *= arg));
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factor = 1;
+  if (num === 0) {
+    return factor;
+  };
+  for (let i = num; i > 0; i--) {
+    factor *= i;
+  };
+  return factor;
 };
 
 // Do not edit below this line
