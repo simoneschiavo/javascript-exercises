ad85e39b588667105ca84b09a5b4cdad1a2c25cc
Simone Schiavo <simoneschiavo@icloud.com>
2024-04-25 14:21:43 +0200

    Add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+        return 'ERROR'
+    }
+    
+    let sum = 0;
+    let initialNum = num1;
+    let finalNum = num2;
 
+    if (num1 > num2) {
+        initialNum = num2;
+        finalNum = num1;
+    };
+
+    for (let i = initialNum; i <= finalNum; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
