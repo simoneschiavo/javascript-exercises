23f32f14ff55466d46e5d5a5f36f69fe17f93b04
Simone Schiavo <simoneschiavo@icloud.com>
2024-04-25 14:47:41 +0200

    Add solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+// rounding to 1 decimal place: Math.round(num * 10) / 10
+
+// from fahrenheit to celsius: (x - 32) * 5/9
+
+const convertToCelsius = function (fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// from celsius to fahrenheit: (x * 9/5 + 32)
+
+const convertToFahrenheit = function (celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * 9 / 5 + 32);
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
