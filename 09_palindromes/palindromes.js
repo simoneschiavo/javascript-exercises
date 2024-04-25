876debdad6b1e4c09176a0e125bdc0e7373b6455
Simone Schiavo <simoneschiavo@icloud.com>
2024-04-25 16:22:54 +0200

    Add solution for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedString = string.toLowerCase().replace(/\W/g, "");
+    let reversedCleanedString = cleanedString.split("").reverse().join("");
+    return (cleanedString === reversedCleanedString);
 };
 
 // Do not edit below this line
