d719c4c3526204a10fd3f701a5954201032b0311
Simone Schiavo <simoneschiavo@icloud.com>
2024-05-01 09:33:57 +0200

    Refactor exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,31 +1,17 @@
-const add = function(...args) {
-  let sum = 0;
-  const allArgsArr = args.flat();
-  allArgsArr.forEach(arg => sum += arg);
-  return sum;
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function(...args) {
-  const allArgsArr = args.flat();
-  let subtraction = allArgsArr[0];
-  for (let i = 1; i < allArgsArr.length; i++) {
-    subtraction -= allArgsArr[i];
-  };
-  return subtraction;
+const subtract = function(a, b) {
+  return a - b;
 };
 
 const sum = function(...args) {
-	let sum = 0;
-  const allArgsArr = args.flat();
-  allArgsArr.forEach((arg) => (sum += arg));
-  return sum;
+  return args.flat().reduce((total, current) => total + current, 0);
 };
 
 const multiply = function(...args) {
-  let multiplication = 1;
-  const allArgsArr = args.flat();
-  allArgsArr.forEach((arg) => (multiplication *= arg));
-  return multiplication;
+  return args.flat().reduce((total, current) => total * current, 1);
 };
 
 const power = function(num1, num2) {
