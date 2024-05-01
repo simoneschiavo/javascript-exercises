59a904daf1c4e33859ef4ac68c05f53500791a62
Simone Schiavo <simoneschiavo@icloud.com>
2024-05-01 10:15:04 +0200

    Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestArr = people.sort((lastPerson, nextPerson) => {
+        const lastPersonAge = getPersonAge(lastPerson);
+        const nextPersonAge = getPersonAge(nextPerson);
+        return lastPersonAge > nextPersonAge ? -1 : 1;
+    });
+    return oldestArr[0];
+};
 
+const getPersonAge = function(person) {
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+    };
+    return person.yearOfDeath - person.yearOfBirth;
 };
 
 // Do not edit below this line
